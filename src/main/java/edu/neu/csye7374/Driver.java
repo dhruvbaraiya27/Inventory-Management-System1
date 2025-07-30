a34f8392f0228e2ebb59c086f7e0032a731be73c
Smit Patel <patel.smit5@northeastern.edu>
2025-07-29 21:52:04 -0400

    added userController userRepository and config files

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -1,5 +1,6 @@
 package edu.neu.csye7374;
 
+import edu.neu.csye7374.repository.UserRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -13,8 +14,8 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
 
-         //Add your code in between these two print statements
-		
+		// Single line call to run the complete Inventory Management System demo
+		new UserRepository().runInventoryManagementDemo();
 		 
 		System.out.println("\n\n============Main Execution End===================");
 	}
